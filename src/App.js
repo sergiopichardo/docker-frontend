0aa4a02ee4fd6b4244b6b87809025276eb44510e
Sergio Pichardo <sp88work@gmail.com>
2020-04-30 15:21:50 -0500

    change text in p tag for testing

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
             <header className="App-header">
                 <img src={logo} className="App-logo" alt="logo" />
                 <p>
-                    Hello World!
+                    Hola Mundo!
                 </p>
                 <a
                     className="App-link"
